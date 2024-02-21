0ebdc16fecb542b2fc5e30e213226cde1fd1aac0
UkReaksa <khsa598@gmail.com>
2024-02-21 21:11:02 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <h1>reaksa</h1>
+      <input type="text"><br>
+      <button>search</button>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
